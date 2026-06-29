0d50434cc69fa9335564fe5281ef0fba7f5497bc
Ysgramor501 <pedroac6@al.insper.edu.br>
2026-06-29 00:28:36 -0300

    ADDED: chrome driver install upon api image bulding, debugging now with proper reload flag

--- backend/debugging-entrypoint.sh
+++ backend/debugging-entrypoint.sh
@@ -3,4 +3,4 @@
 set -e
 
 python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m \
-    uvicorn app.main:app --host 0.0.0.0 --port 8080
+    uvicorn app.main:app --host 0.0.0.0 --port 8080 --reload
